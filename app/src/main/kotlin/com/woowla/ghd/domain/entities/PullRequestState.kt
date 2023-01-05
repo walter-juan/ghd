56cd6a464cd0770925393b5d88cb2a31b4299097
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2023-01-05 23:41:49 +0100

    Pull request comparator
    - Re-order pull request states for sorting
    - Use default state sorting in pull requests

--- app/src/main/kotlin/com/woowla/ghd/domain/entities/PullRequestState.kt
+++ app/src/main/kotlin/com/woowla/ghd/domain/entities/PullRequestState.kt
@@ -1,9 +1,13 @@
 package com.woowla.ghd.domain.entities
 
+/**
+ * The order for this enum values is important
+ * as it is used for sorting, see [PullRequest.defaultComparator]
+ */
 enum class PullRequestState {
+    UNKNOWN,
     OPEN,
+    DRAFT,
     CLOSED,
     MERGED,
-    DRAFT,
-    UNKNOWN,
 }
