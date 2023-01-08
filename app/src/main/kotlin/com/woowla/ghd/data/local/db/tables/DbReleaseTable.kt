5769075385c45b0218cb1afda566cb45d7b870a3
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2023-01-08 19:14:42 +0100

    Rename database tables and columns

--- app/src/main/kotlin/com/woowla/ghd/data/local/db/tables/DbReleaseTable.kt
+++ app/src/main/kotlin/com/woowla/ghd/data/local/db/tables/DbReleaseTable.kt
@@ -4,13 +4,13 @@ import com.woowla.ghd.data.local.db.utils.TextIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
-object DbReleaseTable : TextIdTable() {
+object DbReleaseTable : TextIdTable(name = "release") {
     val name = text("name").nullable()
-    val tagName = text("tagName").nullable()
+    val tagName = text("tag_name").nullable()
     val url = text("url").nullable()
-    val publishedAt = timestamp("publishedAt").nullable()
-    val authorLogin = text("authorLogin").nullable()
-    val authorUrl = text("authorUrl").nullable()
-    val authorAvatarUrl = text("authorAvatarUrl").nullable()
-    val repoToCheckId = reference("repoToCheckId", DbRepoToCheckTable, onDelete = ReferenceOption.CASCADE)
+    val publishedAt = timestamp("published_at").nullable()
+    val authorLogin = text("author_login").nullable()
+    val authorUrl = text("author_url").nullable()
+    val authorAvatarUrl = text("author_avatar_url").nullable()
+    val repoToCheckId = reference("repo_to_check_id", DbRepoToCheckTable, onDelete = ReferenceOption.CASCADE)
 }
