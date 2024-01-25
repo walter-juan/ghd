eb18a129f861731a7339b009eb0ee7ec567a8dc6
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-01-25 10:12:12 +0100

    Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Security
 
 ## [1.4.1]
+### Changed
+- Update dependencies
 
 ## [1.4.0]
 ### ⚠️ IMPORTANT ⚠️
--- app/src/main/kotlin/com/woowla/ghd/presentation/components/IconCard.kt
+++ app/src/main/kotlin/com/woowla/ghd/presentation/components/IconCard.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
