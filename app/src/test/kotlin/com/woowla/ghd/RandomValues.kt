e9652c8543a8673e9c47e381a1e53ed87eea50bf
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-11-23 11:10:14 +0100

    Add random values and entities test utilities

---
+++ app/src/test/kotlin/com/woowla/ghd/RandomValues.kt
@@ -0,0 +1,31 @@
+package com.woowla.ghd
+
+import kotlinx.datetime.Instant
+import java.util.UUID
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.random.nextLong
+
+object RandomValues {
+    val alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    fun randomBoolean(): Boolean = Random.nextBoolean()
+
+    fun randomLong(range: LongRange = 0L..999L): Long = Random.nextLong(range)
+    fun randomInt(range: IntRange = 0..999): Int = Random.nextInt(range)
+
+    fun randomString(size: Int = 2): String = List(size) { alphabet.random() }.joinToString(separator = "")
+
+    fun randomUUID(): UUID = UUID.randomUUID()
+    fun randomId(): String = randomUUID().toString()
+
+    fun randomUrl(): String = "https://${randomString(randomInt(3..15))}.com"
+
+    fun randomInstant(): Instant {
+        val minEpochSeconds = Instant.DISTANT_PAST.epochSeconds
+        val maxEpochSeconds = Instant.DISTANT_FUTURE.epochSeconds
+
+        val randomEpochSeconds = Random.nextLong(minEpochSeconds, maxEpochSeconds + 1)
+        return Instant.fromEpochSeconds(randomEpochSeconds)
+    }
+}
