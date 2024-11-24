2ae0636ac869bb3a1243191bdaddd060917c6215
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-11-24 18:37:32 +0100

    Fix dependency update task
    disallow release candidates as upgradable versions from stable versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -144,14 +144,14 @@ tasks.withType<Test>().configureEach {
 
 tasks.withType<DependencyUpdatesTask> {
     fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
         val regex = "^[0-9,.v-]+(-r)?$".toRegex()
         val isStable = stableKeyword || regex.matches(version)
         return isStable.not()
     }
 
     rejectVersionIf {
-        isNonStable(candidate.version)
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
 
     checkForGradleUpdate = true
