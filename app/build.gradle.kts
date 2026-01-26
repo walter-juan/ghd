c70a63c874cb6faf9acd7ba6e7fe1290a4912754
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2026-01-26 15:45:34 +0100

    Changelog and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "com.woowla"
-version = "2.0.6-beta02"
+version = "2.0.6-beta03"
 // Required for JPackage, as it doesn't accept additional suffixes after the version.
 val versionSimplified = version.toString().substringBefore("-")
 val debug = (extra["debugConfig"] as String).toBoolean()
